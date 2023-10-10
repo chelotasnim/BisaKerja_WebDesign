80e27362e08941510e8f4bc3c7373249c8d2f3a9
chelotasnim <grumpfyxylozx@gmail.com>
2023-10-10 07:54:09 +0700

    Finish

--- Chelo Tasnim Haryono_Website/scripts/p_detail.js
+++ Chelo Tasnim Haryono_Website/scripts/p_detail.js
@@ -1,6 +1,6 @@
 const url = window.location.href.split('?');
 
-const loker = [
+const proyek_data = [
     {
         owner: 'Geprek Kak Rose Malang',
         name: 'Web Promosi Produk',
@@ -42,26 +42,68 @@ const loker = [
             'Asset dapat menggunakan yang tersedia di internet namun real picture'
         ],
         comp_desc: 'Disparpora, Dinas Pariwisata Pemuda dan Olahraga Kabupaten Bondowoso di Jl. A. Yani No.33 Bondowoso (0332) 421475. Mengembangkan potensi pariwisata bondowoso "The Highland Paradise".'
+    },
+    {
+        owner: 'Abhista Farrel',
+        name: 'PowerPoint Evaluasi Produk',
+        cat: 'PowerPoint',
+        up: 'Diunggah 7 Hari Yang Lalu',
+        end: '29 November 2023',
+        range: 'Rp 250.000 - Rp 750.000',
+        ask: '27 Orang',
+        desc: [
+            'Tema evaluasi produk berfokus di bidang konstruksi',
+            'Tema warna sederhana menyesuaikan konten',
+            'Assets dan konten saya berikan'
+        ],
+        req: [
+            'Materi singkat dapat terlihat jelas dan padat',
+            'Desain tidak merusak pengalaman pengguna membaca materi',
+            'Dapat menggunakan ms ppt atau google slide',
+            'Asset dapat menggunakan yang tersedia di internet'
+        ],
+        comp_desc: 'Abhista Farrel, siswa kelas 12 Rekayasa Perangkat Lunak SMK Negeri 1 Bondowoso. Jl. HOS Cokroaminoto No. 110 Kademangan Bondowoso, Jawa Timur.'
+    },
+    {
+        owner: 'Dolant Kreatif Bondowoso',
+        name: 'Desain Stok Logo',
+        cat: 'Desain Logo',
+        up: 'Diunggah 14 Oktober 2023',
+        end: '31 November 2023',
+        range: 'Rp 5.000.000 - Rp 12.000.000',
+        ask: '51 Orang',
+        desc: [
+            'Tema logo thin outline',
+            'Tema warna monotone',
+            'Logo memiliki unsur kartunis 2D',
+            'Logo akan digunakan di media gelap dan terang'
+        ],
+        req: [
+            'Bentuk logo padat dan menyatu',
+            'Warna utama menyesuaikan latar dengan semantik warna tertentu',
+            'Logo tidak boleh diambil dari internet'
+        ],
+        comp_desc: 'Dolant Kreatif Indonesia adalah perusahaan multimedia dan animasi yang berlokasi di Bondowoso, Jawa Timur. Kami telah memproduksi berbagai jenis film animasi yang kami tayangkan di YouTube dan menerima berbagai macam tanggapan positif melalui banyaknya pelanggan yang kami dapatkan di setiap saluran kami.'
     }
 ];
 
 let used_index = url[1] - 1;
-if (url[1] == undefined || loker[url[1] - 1] == undefined) {
+if (url[1] == undefined || proyek_data[url[1] - 1] == undefined) {
     used_index = 0;
 };
 
-for (let key in loker[used_index]) {
+for (let key in proyek_data[used_index]) {
     if (key == 'desc' || key == 'req') {
         let new_list = '';
-        loker[used_index][key].forEach(li => {
+        proyek_data[used_index][key].forEach(li => {
             new_list += `<li>${li}</li>`;
         });
         document.querySelector(`#d-${key}`).innerHTML = new_list;
     } else if (key == 'logo') {
-        document.querySelector(`#d-${key}`).setAttribute('src', `../../Chelo Tasnim Haryono_Bahan/images/${loker[used_index][key]}`);
+        document.querySelector(`#d-${key}`).setAttribute('src', `../../Chelo Tasnim Haryono_Bahan/images/${proyek_data[used_index][key]}`);
     } else {
         document.querySelectorAll(`.d-${key}`).forEach(e => {
-            e.textContent = loker[used_index][key];
+            e.textContent = proyek_data[used_index][key];
         });
     };
 };
